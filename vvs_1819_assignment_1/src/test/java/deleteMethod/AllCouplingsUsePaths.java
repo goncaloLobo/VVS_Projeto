73bc1b08b2c31ec4fe7c7a69f143d32960d3bb81
goncaloLobo <g.ferreiralobo@gmail.com>
2019-04-29 18:32:53 +0100

    Inicio AllCouplingsUsePaths
    Delete - feito
    ProposePosition - feito
    compact - não feito

---
+++ vvs_1819_assignment_1/src/test/java/deleteMethod/AllCouplingsUsePaths.java
@@ -0,0 +1,79 @@
+package deleteMethod;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import sut.ArrayNTree;
+
+public class AllCouplingsUsePaths {
+
+	// caminho: 1,5,6
+	@Test
+	public void testDeleteSmaller() {
+		List<Integer> list = Arrays.asList(1, 5, 10, 15);
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 3);
+
+		tree.delete(3);
+		int size = tree.size();
+		assertEquals(3, size, "delete");
+	}
+
+	// caminho: 1,5,7,8,1,3
+	@Test
+	public void testDeleteBigger() {
+		List<Integer> list = Arrays.asList(1, 5, 10, 15);
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 3);
+
+		tree.delete(20);
+		int size = tree.size();
+		assertEquals(3, size, "delete");
+	}
+
+	// caminho: 1,2,6,7,2,4,3
+	@Test
+	public void testDeleteMiddleElem() {
+		List<Integer> list = Arrays.asList(1, 5, 10, 15);
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 3);
+
+		tree.delete(10);
+		int size = tree.size();
+		assertEquals(3, size, "delete");
+	}
+
+	// caminho: 1,2,4,9 (dentro do proposePosition)
+	@Test
+	public void testDeleteCap() {
+		List<Integer> list = Arrays.asList(1, 5, 10);
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 3);
+
+		tree.delete(5);
+		int size = tree.size();
+		assertEquals(3, size, "delete");
+	}
+
+	// caminho: 1,2,6,7,2,5,3 (dentro do proposePosition)
+	@Test
+	public void testDeleteFive() {
+		List<Integer> list = Arrays.asList(1, 5, 10, 15);
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 3);
+
+		tree.delete(6);
+		int size = tree.size();
+		assertEquals(3, size, "delete");
+	}
+	
+	// caminho: 1,2,6,7,2,6,7,2,5,3
+	@Test
+	public void testDeleteIndexTwice() {
+		List<Integer> list = Arrays.asList(1, 5, 10, 15);
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 3);
+
+		tree.delete(11);
+		int size = tree.size();
+		assertEquals(3, size, "delete");
+	}
+}
